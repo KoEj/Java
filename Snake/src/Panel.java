f84f1fd018a8ab1c4099f36ddc5db8935427f7da
Paweł Niedziółka <249022@student.pwr.edu.pl>
2021-06-14 19:50:55 +0200

    v1.1 threads

--- Snake/src/Panel.java
+++ Snake/src/Panel.java
@@ -2,9 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.lang.*;
 
 public class Panel extends JPanel implements ActionListener {
 
+    public int SPEED = 50;
     static int APP_W = 800;
     static int APP_H = 800;
     static int APP_U = 25;
@@ -12,6 +16,7 @@ public class Panel extends JPanel implements ActionListener {
     int score = 0;
     int food_pos_x;
     int food_pos_y;
+    int thread_working = 1;
     int[] x = new int[GAM_EL];
     int[] y = new int[GAM_EL];
     int snake_el = 10;
@@ -20,6 +25,7 @@ public class Panel extends JPanel implements ActionListener {
 
     Random random;
     Timer timer;
+    Thread thread;
 
     Panel() {
         setPreferredSize(new Dimension(APP_W,APP_H));
@@ -34,8 +40,27 @@ public class Panel extends JPanel implements ActionListener {
     public void start() {
         running = "YES";
         new_food();
-        timer = new Timer(75,this);
+        timer = new Timer(SPEED,this);
         timer.start();
+
+        thread = new Thread(() ->
+        {
+            while(thread_working==1) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                SPEED = random.nextInt(50) + 50;
+                System.out.println("check");
+                timer.stop();
+                timer = new Timer(SPEED, this);
+                timer.start();
+            }
+        });
+
+        thread.start();
     }
 
     public void paint(Graphics graphics) {
@@ -44,11 +69,6 @@ public class Panel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics graphics) {
-
-//        for(int i=0;i<APP_H/APP_U;i++) {
-//            graphics.drawLine(i*APP_U,0,i*APP_U,APP_H);
-//            graphics.drawLine(0,i*APP_U,APP_W,i*APP_U);
-//        }
         if("YES".equals(running)) {
             graphics.setColor(Color.red);
             graphics.fillOval(food_pos_x, food_pos_y, APP_U, APP_U);
@@ -98,6 +118,7 @@ public class Panel extends JPanel implements ActionListener {
                  y[0]<0 || y[0]>APP_H) {
                 running = "NO";
                 timer.stop();
+                thread_working=0;
                 break;
             }
         }
@@ -152,6 +173,7 @@ public class Panel extends JPanel implements ActionListener {
             }
         }
     }
+
 }
 
 
